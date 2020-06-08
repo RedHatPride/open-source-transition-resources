37572dc5ecd94fbede7b9c2c205a48b27e52f00d
CodePoetSarah <sarah.annie.thornton@gmail.com>
2020-06-08 13:05:19 -0400

    Added UK guide script

--- publish.sh
+++ publish.sh
@@ -2,3 +2,4 @@
 #
 asciidoctor transition-resources/master.adoc -D website/static/guide/html -o index.html
 asciidoctor-pdf transition-resources/master.adoc -D website/static/guide/pdf -o transgender-resources.pdf
+asciidoctor-pdf transition-resources/uk-guide.adoc -D website/static/guide/pdf -o transgender-resources-uk.pdf
