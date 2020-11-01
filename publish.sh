ba5ba0ddb2a448e04de8ad886a1d43deb2578d60
CodePoetSarah <sarah.annie.thornton@protonmail.com>
2020-11-01 16:34:03 -0500

    Added a releases folder and updated the publish script so we use github for compiled PDFs until we have the website up and running.

--- publish.sh
+++ publish.sh
@@ -6,3 +6,8 @@ asciidoctor transition-resources/index.adoc -D website/static/guide/html -o inde
 asciidoctor-pdf transition-resources/index.adoc -D website/static/guide/pdf -o transgender-resources.pdf
 asciidoctor-pdf transition-resources/uk-guide.adoc -D website/static/guide/pdf -o uk-transgender-resources.pdf
 asciidoctor-pdf transition-resources/apac-guide.adoc -D website/static/guide/pdf -o apac-transgender-resources.pdf
+
+# Rebuild them for the releases folder
+asciidoctor-pdf transition-resources/index.adoc -D releases -o transgender-resources.pdf
+asciidoctor-pdf transition-resources/uk-guide.adoc -D releases -o uk-transgender-resources.pdf
+asciidoctor-pdf transition-resources/apac-guide.adoc -D releases -o apac-transgender-resources.pdf
