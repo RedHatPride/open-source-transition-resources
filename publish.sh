ffaa9def82530e55406298bf88e02d08eef3a548
Michael Scherer <misc@redhat.com>
2021-01-22 16:36:23 +0100

    Refactor the publish script
    This will ease the addition of more guide later

--- publish.sh
+++ publish.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #
+REGIONS="uk apac"
+
 asciidoctor transition-resources/index.adoc -D website/static/guide/html -o index.html
-
 # Build the PDFs
-asciidoctor-pdf transition-resources/index.adoc -D website/static/guide/pdf -o transgender-resources.pdf
-asciidoctor-pdf transition-resources/uk-guide.adoc -D website/static/guide/pdf -o uk-transgender-resources.pdf
-asciidoctor-pdf transition-resources/apac-guide.adoc -D website/static/guide/pdf -o apac-transgender-resources.pdf
+for dest in website/static/guide/pdf releases ; do 
+    asciidoctor-pdf transition-resources/index.adoc -D $dest -o transgender-resources.pdf
+    for g in $REGIONS; do
+       asciidoctor-pdf transition-resources/${g}-guide.adoc -D $dest -o ${g}-transgender-resources.pdf
+    done
+done
 
-# Rebuild them for the releases folder
-asciidoctor-pdf transition-resources/index.adoc -D releases -o transgender-resources.pdf
-asciidoctor-pdf transition-resources/uk-guide.adoc -D releases -o uk-transgender-resources.pdf
-asciidoctor-pdf transition-resources/apac-guide.adoc -D releases -o apac-transgender-resources.pdf
