f4ca416befaccf27d93b29c0f3b0c5e9329d66e5
Michael Scherer <misc@redhat.com>
2021-01-24 11:16:08 +0100

    Add $OPTIONS, to be able to pass options to asciidoctor

--- publish.sh
+++ publish.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 #
 REGIONS="uk apac"
+OPTIONS=
 
-asciidoctor transition-resources/index.adoc -D website/static/guide/html -o index.html
+asciidoctor $OPTIONS transition-resources/index.adoc -D website/static/guide/html -o index.html
 # Build the PDFs
 for dest in website/static/guide/pdf releases ; do 
-    asciidoctor-pdf transition-resources/index.adoc -D $dest -o transgender-resources.pdf
+    asciidoctor-pdf $OPTIONS transition-resources/index.adoc -D $dest -o transgender-resources.pdf
     for g in $REGIONS; do
-       asciidoctor-pdf transition-resources/${g}-guide.adoc -D $dest -o ${g}-transgender-resources.pdf
+       asciidoctor-pdf $OPTIONS transition-resources/${g}-guide.adoc -D $dest -o ${g}-transgender-resources.pdf
     done
 done
 
