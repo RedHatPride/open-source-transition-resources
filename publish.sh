c33c0c8d698192a2e5e2c57aaa615bb2e069505e
Sarah Thornton <sarah.thornton@redhat.com>
2021-06-05 19:19:04 -0400

    Removed Website
    As we are sticking with github, I removed the website from the repo
    and the line in the publish script to generate the html

--- publish.sh
+++ publish.sh
@@ -3,7 +3,6 @@
 REGIONS="uk apac"
 OPTIONS="-l $PWD/library -r tel-inline-macro"
 
-asciidoctor $OPTIONS transition-resources/index.adoc -D website/static/guide/html -o index.html
 # Build the PDFs
 for dest in website/static/guide/pdf releases ; do 
     asciidoctor-pdf $OPTIONS transition-resources/index.adoc -D $dest -o transgender-resources.pdf
